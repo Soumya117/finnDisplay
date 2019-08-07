696e2f1f2f0e012e8c3f6968272dc630127190ed
Soumya <soumya@appeartv.com>
2019-08-07 15:34:15 +0200

    change sold view

--- links.py
+++ links.py
@@ -12,7 +12,7 @@ def jsonToHtml(jsonStr, inputDate):
             </th>
             <td height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{address}</font></td>
             <td height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{area}</font></td>
-            <td height="40"  bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{price}</font></td>
+            <td height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{price}</font></td>
             """.format(link=item['link'],
                        text=item['text'],
                        address=item['address'],
--- sold.py
+++ sold.py
@@ -7,12 +7,12 @@ def jsonToHtml(jsonStr, inputDate):
         day = item['time'].split('T')
         if inputDate in day:
             tstr2 = """<tr>
-            <th bgcolor="#2a3c3c" height="35">
+            <th bgcolor="#2a3c3c" height="40" width="40%">
             <a href="{link}" style="color:#FFFFFF;">{text}</a>
             </th>
-            <td  height="35" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{address}</font></td>
-            <td  height="35" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{price}</font></td>
-            <td  height="35" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{area}</font></td>
+            <td  height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{address}</font></td>
+            <td  height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{price}</font></td>
+            <td  height="40" bgcolor="#2a3c3c" style="padding-right: 25px;padding-left: 25px;"><font size="3" color="white">{area}</font></td>
             """.format(link=item['link'], address=item['address'], area=item['area'], text=item['text'], price=item['price'])
             tstr1 += tstr2
             tstr4="""</tr>"""
